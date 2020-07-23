5e991c07a36d1fb06ef05ff3926fcc86be12b1fb
rydesun <rydesun@gmail.com>
2020-07-23 10:16:40 +0800

    test(config): add unit test for parser

---
+++ exch/config/parser_test.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/rydesun/awesome-github/exch/github"
+	"github.com/stretchr/testify/require"
+)
+
+const testAccessToken = "123456"
+
+func TestMain(m *testing.M) {
+	oldAccessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
+	err := os.Setenv("GITHUB_ACCESS_TOKEN", testAccessToken)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	code := m.Run()
+	os.Setenv("GITHUB_ACCESS_TOKEN", oldAccessToken)
+	os.Exit(code)
+}
+
+func TestGetConfig(t *testing.T) {
+	require := require.New(t)
+
+	path := "../../configs/config.yaml"
+	yamlParser, err := NewYAMLParser(path)
+	require.Nil(err)
+	actual, err := GetConfig(yamlParser)
+	require.Nil(err)
+
+	expected := Config{
+		ConfigPath:    path,
+		AccessToken:   testAccessToken,
+		MaxConcurrent: 3,
+		StartPoint: StartPoint{
+			Path: "avelino/awesome-go",
+			Id: github.RepoID{
+				Owner: "avelino",
+				Name:  "awesome-go",
+			},
+		},
+		Log: Loggers{
+			Main: Logger{
+				Path: []string{"/tmp/awesome-github.log"},
+			},
+			Http: Logger{
+				Path: []string{"/tmp/awesome-github.log"},
+			},
+		},
+	}
+	require.Equal(expected, actual)
+
+	// Invalid config path
+	_, err = NewYAMLParser("")
+	require.NotNil(err)
+	_, err = NewYAMLParser("a/b/c/d")
+	require.NotNil(err)
+}
