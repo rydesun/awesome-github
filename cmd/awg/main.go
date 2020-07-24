4d14363f6722d82c2ab650011387c0ba6073a991
rydesun <rydesun@gmail.com>
2020-07-25 07:09:55 +0800

    feat(cmd): new cli

--- cmd/awg/main.go
+++ cmd/awg/main.go
@@ -4,25 +4,53 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/urfave/cli/v2"
+
 	"github.com/rydesun/awesome-github/exch/config"
 )
 
 func main() {
+	app := &cli.App{
+		Name:  "awg",
+		Usage: "Awesome GitHub repositories",
+		Commands: []*cli.Command{
+			{
+				Name:   "fetch",
+				Usage:  "Fetch data about awesome repositories",
+				Action: fetch,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:      "config",
+						Aliases:   []string{"c"},
+						Usage:     "YAML config",
+						Required:  true,
+						TakesFile: true,
+					},
+				},
+			},
+		},
+		EnableBashCompletion: true,
+	}
+	app.Run(os.Args)
+}
+
+func fetch(c *cli.Context) error {
 	// CLI
 	writer := os.Stdout
+	configPath := c.String("config")
 
 	// Always parse config first.
-	config, err := parseConfig()
+	config, err := parseConfig(configPath)
 	if err != nil {
 		fmt.Fprintln(writer, err)
-		os.Exit(1)
+		cli.OsExiter(1)
 	}
 
 	// Set loggers right now.
 	logger, err := setLoggers(config)
 	if err != nil {
 		fmt.Fprintln(writer, err)
-		os.Exit(1)
+		cli.OsExiter(1)
 	}
 
 	// The worker takes over all writers,
@@ -31,24 +59,17 @@ func main() {
 	// Must init first.
 	err = worker.Init(config)
 	if err != nil {
-		os.Exit(1)
+		cli.OsExiter(1)
 	}
 	err = worker.Work()
 	if err != nil {
-		os.Exit(1)
+		cli.OsExiter(1)
 	}
+	return nil
 }
 
-func parseConfig() (config.Config, error) {
-	flagParser := config.FlagParser{}
-	flags, err := flagParser.Parse()
-	if err != nil {
-		err = fmt.Errorf("Failed to parse cmd flags. %v", err)
-		return config.Config{}, err
-	}
-	configPath := flags.ConfigPath
-
-	yamlParser, err := config.NewYAMLParser(flags.ConfigPath)
+func parseConfig(configPath string) (config.Config, error) {
+	yamlParser, err := config.NewYAMLParser(configPath)
 	if err != nil {
 		err = fmt.Errorf("Failed to parse config files. %v", err)
 		return config.Config{}, err
@@ -59,6 +80,6 @@ func parseConfig() (config.Config, error) {
 		err = fmt.Errorf("Failed to parse config files. %v", err)
 		return config.Config{}, err
 	}
-	flags.ConfigPath = configPath
+	conf.ConfigPath = configPath
 	return conf, nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213
 	github.com/schollz/progressbar/v3 v3.3.4
 	github.com/stretchr/testify v1.4.0
+	github.com/urfave/cli/v2 v2.2.0
 	go.uber.org/zap v1.15.0
 	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd
 	gopkg.in/yaml.v2 v2.2.2
