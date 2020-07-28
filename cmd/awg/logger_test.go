aac058c826419e16315562b1a0f8045a225c6b9a
rydesun <rydesun@gmail.com>
2020-07-28 13:42:48 +0800

    test(logger): add unit test

---
+++ cmd/awg/logger_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/rydesun/awesome-github/exch/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSetLoggers(t *testing.T) {
+	require := require.New(t)
+	tempFile, err := ioutil.TempFile("", "awg_fake_log_*")
+	require.Nil(err)
+	defer func() {
+		tempFile.Close()
+		os.Remove(tempFile.Name())
+	}()
+	mainLogger, err := setLoggers(config.Loggers{
+		Main: config.Logger{
+			Path: []string{tempFile.Name()},
+		},
+		Http: config.Logger{
+			Path: []string{tempFile.Name()},
+		},
+	})
+	require.Nil(err)
+	mainLogger.Info("test log")
+	err = mainLogger.Sync()
+	require.Nil(err)
+}
