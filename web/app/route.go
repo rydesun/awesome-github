90519fb60367f9aa2da3a72b21adea7e8c63c291
rydesun <rydesun@gmail.com>
2020-08-04 19:13:20 +0800

    feat(server): support remote resources

--- web/app/route.go
+++ web/app/route.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/gofiber/cors"
@@ -45,12 +46,24 @@ func (r *Router) Init(repoID github.RepoID, scriptPath, dataPath string) error {
 func (r *Router) Route() {
 	app := r.app
 	app.Use(cors.New())
+
+	dataPath := r.dataPath
+	scriptPath := r.scriptPath
+	urlData, _ := url.Parse(dataPath)
+	urlScript, _ := url.Parse(scriptPath)
+	if urlData.Scheme != "http" && urlData.Scheme != "https" {
+		app.Static("/data", dataPath)
+		dataPath = "/data"
+	}
+	if urlScript.Scheme != "http" && urlScript.Scheme != "https" {
+		app.Static("/js", scriptPath)
+		scriptPath = "/js"
+	}
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Set("content-type", "text/html; charset=utf-8")
-		c.Send(wrapReadme(r.html, "/data", "/js"))
+		c.Send(wrapReadme(r.html, dataPath, scriptPath))
 	})
-	app.Static("/js", r.scriptPath)
-	app.Static("/data", r.dataPath)
+
 	app.Listen(r.listen)
 }
 
